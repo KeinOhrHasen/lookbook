00fe11dc3a98cf5372293978b5c5b45298a880bf
Yurii <yuriy.zitynyuk@kindgeek.com>
2024-04-04 16:06:46 +0300

    Clean code

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
-    
+
     return <div className="w-full py-16 px-6 lg:py-24 bg-black">
       <div className="text-white text-xs text-center">
         © 2024 Lookbook Inc. Made with smile in Lviv.
