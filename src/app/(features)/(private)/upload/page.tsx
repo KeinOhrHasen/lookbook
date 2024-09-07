48e24746b64dd8028d326ed51fd4cb82f2d70056
Yurii <yuriy.zitynyuk@kindgeek.com>
2024-09-07 15:47:47 +0300

    Fix file path

--- src/app/(features)/(private)/upload/page.tsx
+++ src/app/(features)/(private)/upload/page.tsx
@@ -3,7 +3,7 @@
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import Image from 'next/image';
-import mountain from '../../../../public/images/mountain.jpg';
+import mountain from '../../../../../public/images/mountain.jpg';
 import { Button } from '@/components/ui/button';
 import { useRef } from 'react';
 import axios from 'axios';
