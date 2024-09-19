76b9eafa9805f8e1b699a49c4694dff945bcbed9
Yurii <yuriy.zitynyuk@kindgeek.com>
2024-09-19 16:12:54 +0300

    Update gitignore

--- src/app/(features)/(private)/grids/page.tsx
+++ src/app/(features)/(private)/grids/page.tsx
@@ -2,8 +2,7 @@
 
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
-import { ENVIRONMENT } from '../../../../core/configs/environment';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import GridList from './_grid-list';
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '@/lib/redux/hook';
@@ -14,29 +13,12 @@ export const GridsContext = React.createContext([]);
 
 export default function Grid() {
   const router = useRouter();
-  const [data, setData] = useState([]);
   const grids = useAppSelector(selectGrids);
   const dispatch = useAppDispatch();
+
   useEffect(() => {
-    // fetchGrids();
     dispatch(list());
   }, []);
-
-  async function fetchGrids(): void {
-    try {
-      const resp = await fetch(`${ENVIRONMENT.apiURL}/grids`, {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      });
-      const grids = await resp.json();
-      setData(grids);
-    } catch {
-      throw new Error('Can`t load grids');
-    }
-  }
 
   function addNewGrid() {
     router.push('/grids/new');
