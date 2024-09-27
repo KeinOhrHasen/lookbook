a67a1d772df5b31ac150c28a344a451f81c3f031
Yurii <yuriy.zitynyuk@kindgeek.com>
2024-09-27 22:59:20 +0300

    Add chat context

--- src/app/(features)/(private)/chat/page.tsx
+++ src/app/(features)/(private)/chat/page.tsx
@@ -7,14 +7,31 @@ import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { fetchUserTokensById, generateChatResponse, subtractTokens } from '@/src/core/utils/actions';
 
+export enum Role {
+  user = 'user',
+  system = 'system',
+  assistant = 'assistant',
+}
+export interface IMessage {
+  role: Role;
+  content: string;
+}
+
 export default function Chat() {
   const [text, setText] = useState('');
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<IMessage[]>([]);
   const userId = 144112;
-  const isPending = false;
 
-  const { mutate } = useMutation({
-    mutationFn: (message: string) => generateChatResponse(message),
+  const { mutate, isPending } = useMutation({
+    mutationFn: (query: IMessage) => generateChatResponse([...messages, query]),
+    onSuccess: (response) => {
+      if (!response) {
+        toast.error('Something went wrong...');
+        return;
+      }
+
+      setMessages((prev) => [...prev, response as IMessage]);
+    },
     // {
     // const currentTokens = await fetchUserTokensById(userId);
 
@@ -37,7 +54,11 @@ export default function Chat() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    mutate(text);
+    const query = { role: Role.user, content: text };
+
+    mutate(query);
+    setMessages((prev) => [...prev, query]);
+    setText('');
   };
 
   return (
@@ -45,7 +66,7 @@ export default function Chat() {
       <div>
         {messages.map(({ role, content }, index) => {
           const avatar = role == 'user' ? '👤' : '🤖';
-          const bcg = role === 'user' ? 'bg-base-200' : 'bg-base-100';
+          const bcg = role === 'user' ? 'bg-slate-300' : 'bg-slate-100';
           return (
             <div key={index} className={`${bcg} flex py-6 -mx-8 px-8 text-xl leading-loose border-b border-base-300`}>
               <span className="mr-4">{avatar}</span>
@@ -53,7 +74,7 @@ export default function Chat() {
             </div>
           );
         })}
-        {isPending ? <span className="loading"></span> : null}
+        {isPending ? <span className="SOME_SPINNER"></span> : null}
       </div>
       <form onSubmit={handleSubmit} className="max-w-4xl pt-12">
         <div className="flex w-full">
--- src/core/utils/actions.tsx
+++ src/core/utils/actions.tsx
@@ -1,27 +1,26 @@
 'use server';
 
-import { ENVIRONMENT } from '@/src/core/configs/environment';
-import axios from 'axios';
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export const generateChatResponse = async (chatMessage) => {
-  const response = await openai.chat.completions.create({
-    messages: [
-      { role: 'system', content: 'Hey there! I am tour assistant' },
-      { role: 'user', content: chatMessage },
-    ],
-    model: 'gpt-3.5-turbo',
-    temperature: 1,
-  });
+export const generateChatResponse = async (chatMessages) => {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [{ role: 'system', content: 'Hey there! I am tour assistant' }, ...chatMessages],
+      model: 'gpt-3.5-turbo',
+      temperature: 1,
+    });
 
-  console.log(response.choices[0].message);
-  console.log(response);
+    console.log(response.choices[0].message);
+    console.log(response);
 
-  return 'awesome';
+    return response.choices[0].message;
+  } catch (error) {
+    return null;
+  }
 };
 
 export const fetchUserTokensById = (a) => {};
